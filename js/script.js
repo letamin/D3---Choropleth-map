2c1165e8166d28c51d966b09af80083d8cee76ec
letamin <lethanhminh0901@gmail.com>
2020-08-02 02:04:36 +0700

    choropleth map updated

--- js/script.js
+++ js/script.js
@@ -2,33 +2,46 @@ Promise.all([
     d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/50m.tsv'),    //Data for the country names (uses the country id to get name)
     d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')   //Data for the countries (inclues the country id to get name later)
 ]).then(([tsvData, topojsonData]) => {
-    appendTooltip();
-    const pathGenerator = initialize();
-    const countryById = getCountryById(tsvData);
-    const countries = topojson.feature(topojsonData, topojsonData.objects.countries);
+    const { pathGenerator, countries, colorLegendGroup } = initialize(tsvData, topojsonData);
 
-    countries.features.forEach(d => {
-        Object.assign(d.properties, countryById[d.id])
+    appendTooltip();
+    render(countries, pathGenerator, colorLegendGroup);
+
+}).catch(err => console.log(err))
+
+//Render function
+const render = (countries, pathGenerator, colorLegendGroup) => {
+    colorScale.domain(countries.features.map(colorValue).sort().reverse())
+        .range(d3.schemeYlOrBr[colorScale.domain().length]);    //Create some colors for our map. More information: https://github.com/d3/d3-scale-chromatic/blob/master/README.md
+
+    colorLegend(colorLegendGroup, {
+        colorScale,
+        circleRadius: 15,
+        legendSpacing: 45,
+        textOffset: 25
     })
 
     const g = d3.select('.svgGroup');
-
     g.selectAll('path').data(countries.features)
         .enter().append('path')
         .attr('class', 'countries')
         .attr('d', pathGenerator)
+        .attr('fill', d => colorScale(colorValue(d)))
         .attr('transform', `translate(265, 100)`)
-        .on('mouseover', d => hoverOver(d, d.properties.name))
+        .on('mouseover', d => hoverOver(d.properties.name, colorValue(d)))
         .on('mouseout', hoverDone)
+}
 
-}).catch(err => console.log(err))
+const colorScale = d3.scaleOrdinal();
+const colorValue = d => d.properties.income_grp; //get the color based on the coutry's income group
 
-function initialize() {
+//Initialize the data for our map
+function initialize(tsvData, topojsonData) {
     const width = document.body.clientWidth;
     const height = document.body.clientHeight;
     const svg = d3.select('svg');
     const g = svg.append('g').attr('class', 'svgGroup');
-    const projection = d3.geoOrthographic();       //For more details on d3 geo: https://github.com/d3/d3-geo
+    const projection = d3.geoStereographic();       //For more details on d3 geo: https://github.com/d3/d3-geo
     const pathGenerator = d3.geoPath().projection(projection);
 
     svg.attr('width', width).attr('height', height);
@@ -38,12 +51,25 @@ function initialize() {
         .attr('d', pathGenerator({ type: 'Sphere' }))
         .attr('transform', `translate(265, 100)`)
 
+    zoomAndPan(g, svg);
+
+    const countriesById = getCountriesByIds(tsvData);
+    const countries = topojson.feature(topojsonData, topojsonData.objects.countries);
+    const colorLegendGroup = svg.append('g').attr('transform', `translate(160, 500)`);
+
+    countries.features.forEach(d => {
+        Object.assign(d.properties, countriesById[d.id])
+    })
+
+    return { pathGenerator, countries, svg, colorLegendGroup };
+}
+
+//Zoom and pan function
+const zoomAndPan = (g, svg) => {
     //For more details on d3 zoom and pan: https://github.com/d3/d3-zoom
     svg.call(d3.zoom().on('zoom', () => {
         g.attr('transform', d3.event.transform)
     }))
-
-    return pathGenerator;
 }
 
 const appendTooltip = () => {
@@ -51,26 +77,52 @@ const appendTooltip = () => {
         .attr("class", "tooltip")
 }
 
-const getCountryById = (tsvData) => {
-    const countryById = tsvData.reduce((name, index) => {
+const getCountriesByIds = (tsvData) => {
+    const countriesById = tsvData.reduce((name, index) => {
         name[index.iso_n3] = index;
         return name
     }, {})
 
-    return countryById
+    return countriesById
 }
 
-const hoverOver = (d, countryName) => {
+//Add tooltip when hover over a country
+const hoverOver = (countryName, countryColor) => {
     const tooltip = d3.select('.tooltip');
-    console.log(countryName)
-    tooltip.html(countryName)
+    tooltip.text(`${countryName}: ${countryColor.split('.')[1]}`)
         .style("visibility", 'visible')
         .style("left", (d3.event.pageX + 10) + "px")
         .style("top", (d3.event.pageY - 15) + "px");
 }
 
+//Remove the tooltip when mouse out
 const hoverDone = () => {
     const tooltip = d3.select('.tooltip');
     tooltip.style("visibility", 'hidden');
 }
 
+//Add legends
+const colorLegend = (selection, props) => {
+    const { colorScale, circleRadius, legendSpacing, textOffset } = props;
+
+    const legendDatajoin = selection.selectAll('g')
+        .data(colorScale.domain().reverse());
+
+    const legendGroups = legendDatajoin
+        .enter().append('g')
+        .attr('class', 'legend')
+        .attr('transform', (_d, i) => `translate(0, ${i * legendSpacing})`);
+
+    //legend color circles
+    legendGroups
+        .append('circle')
+        .attr('r', circleRadius)
+        .attr('fill', colorScale);
+
+    //legend texts
+    legendGroups
+        .append('text')
+        .text(d => d.split('.')[1])
+        .attr('dy', '0.32em')
+        .attr('x', textOffset);
+}
